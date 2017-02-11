cbce5396652e737a7349f8e0c7df0511051dc16e
Taje Carter <taje.carter@mymona.uwi.edu>
2017-02-11 20:40:21 +0000

    Ex2

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ This file creates your application.
 """
 
 from app import app
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 
 
 ###
@@ -34,6 +34,43 @@ def send_text_file(file_name):
     """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
+    
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        from_name = request.form['fullname']
+        from_email = request.form['email']
+        subject = request.form['subject']
+        msg = request.form['message']
+        send_email(from_name, from_email, subject, msg)
+        flash('MESSAGE SENT SUCCESSFULLY')
+        return redirect(url_for('home'))
+        
+    """Render the website's contact page."""
+    return render_template('contact.html')
+
+def send_email(from_name, from_email, subject, msg):
+        import smtplib
+        to_name = 'David'
+        to_addr  = 'username@gmail.com'
+        message = """From: {} <{}>
+        To: {} <{}> 
+        Subject: {}
+        {}
+        """
+        message_to_send = message.format(from_name, from_email, to_name, to_addr, subject, msg)
+        
+        # Credentials (if needed)
+        username = 'username@gmail.com'
+        password = 'password'
+        
+        # The actual mail send
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(from_email, to_addr, message_to_send)
+        server.quit()
+
 
 
 @app.after_request
